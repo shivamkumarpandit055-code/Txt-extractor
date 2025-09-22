0081a3c0bfe37016ed10a137a1a308bbf189d84d
shivamkumarpandit055-code <shivamkumarpandit055@gmail.com>
2025-09-23 01:00:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8250884972:AAEuKfSaIX5B9NyfFjSFi-BkveD5
 OWNER_ID = int(os.environ.get("OWNER_ID", "7276272743"))  # Your Telegram user ID
 SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "7276272743 7276272743").split()))  # Space-separated user IDs
 
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://shivamkumar055gram:JkInylriCfgItXqd@cluster0.tnjashu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")##your mongo url eg: withmongodb+srv://xxxxxxx:xxxxxxx@clusterX.xxxx.mongodb.net/?retryWrites=true&w=majority
+MONGO_URL = os.environ.get("mongodb+srv://shivamkumar055gram:JkInylriCfgItXqd@cluster0.tnjashu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")##your mongo url eg: withmongodb+srv://xxxxxxx:xxxxxxx@clusterX.xxxx.mongodb.net/?retryWrites=true&w=majority
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002838326478"))  # Telegram channel ID (with -100 prefix)
 
 PREMIUM_LOGS = os.environ.get("PREMIUM_LOGS", "-1002838326478")  # Optional here you'll get all logs
